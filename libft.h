07ab42081a4ad84194e22090b188cd8db1e0e5d2
noalexan <noalexan@student.42nice.fr>
2025-01-13 15:24:51 +0100

    Fix `C++` compatibility

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: noalexan <noalexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 04:35:08 by noalexan          #+#    #+#             */
-/*   Updated: 2024/12/15 04:47:06 by noalexan         ###   ########.fr       */
+/*   Updated: 2025/01/13 15:24:19 by noalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,10 +46,10 @@ void	*ft_calloc(size_t nmemb, size_t size);
 // Bonuses
 
 t_list	*ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *elem);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
-void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *elem);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
